75d711797694c595e468959c4041fe44ace21b84
didil <1284255+didil@users.noreply.github.com>
2019-10-04 18:53:44 +0100

    return errs

--- caddyhttp/httpserver/server.go
+++ caddyhttp/httpserver/server.go
@@ -563,13 +563,13 @@ type tcpKeepAliveListener struct {
 func (ln tcpKeepAliveListener) Accept() (c net.Conn, err error) {
 	tc, err := ln.AcceptTCP()
 	if err != nil {
-		return
+		return nil, err
 	}
 	if err = tc.SetKeepAlive(true); err != nil {
-		return
+		return nil, err
 	}
 	if err = tc.SetKeepAlivePeriod(3 * time.Minute); err != nil {
-		return
+		return nil, err
 	}
 	return tc, nil
 }
